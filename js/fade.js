b1c156f738b97f6717227c502de882e4a837d6eb
hawkeynl <hawkey@hawkeynl.com>
2019-12-05 14:24:17 +0100

    Update fade.js

--- js/fade.js
+++ js/fade.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 function fadein() {
     $('.fadein').each(function(i) {
         let bottom_of_object = $(this).position().top + $(this).outerHeight();
-        let bottom_of_window = $(window).scrollTop() + $(window).height() + 200;
+        let bottom_of_window = $(window).scrollTop() + $(window).height() + 150;
         /* If the object is completely visible in the window, fade it it */
         if( bottom_of_window > bottom_of_object ){
             $(this).animate({'opacity':'1'},750);

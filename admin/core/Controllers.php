e53e5ef845d24609c4499ead92d37b7ddd9d4ac0
hawkeynl <hawkey@hawkeynl.com>
2019-12-06 15:59:40 +0100

    Added Controllers

---
+++ admin/core/Controllers.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . "../php/Database.php";
+
+class Controllers
+{
+    public function login(string $username, string $password) {
+
+    }
+
+    public function register(string $username, string $email, string $password) {
+
+    }
+}
